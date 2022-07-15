c09f9376c17c970c8118ca6b6b131e32cafdf0cf
Julian Henao DMS <julian.henao@dms.ms>
2022-07-15 08:30:02 -0500

    Configuración inicial de tablas y columnas pdf, uso de lista, margenes y alineado. En proceso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PdfMakeHtmlComponent } from './components/pdf-make-html/pdf-make-html.component';
+import { PdfMakeTestComponent } from './components/pdf-make-test/pdf-make-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'jspdf-pdfmake', component: PdfMakeHtmlComponent},
+  {path: 'only-pdfmake', component: PdfMakeTestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
